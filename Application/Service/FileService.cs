936b878dbd69f50c64f61ed63aefdcc2da4bf22e
Talles Dias <tallesdelfino1@gmail.com>
2022-11-03 14:36:46 -0300

    começando a manipulação de arquivo

---
+++ Application/Service/FileService.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTO;
+using Application.Service.Interface;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Service
+{
+    public class FileService : IFileService
+    {
+        private readonly string basePath;
+        private readonly IHttpContextAccessor context;
+
+        public FileService(IHttpContextAccessor context)
+        {
+            this.context = context;
+            basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailDTO>> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailDTO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Service/Interface/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTO;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service.Interface
+{
+    public interface IFileService
+    {
+        public byte[] GetFile(string fileName);
+        public Task<FileDetailDTO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailDTO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
